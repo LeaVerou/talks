39464369a4c78236d990e3a06f265ddda3f9bf64
Lea Verou <lea@verou.me>
2016-09-26 14:40:53 -0400

    Checkpoint

--- js/talk.js
+++ js/talk.js
@@ -1,7 +1,4 @@
-var slideshow = new SlideShow();
-var $ = Bliss;
-
-(function($, $$){
+var $ = Bliss, $$ = Bliss.$;
 
 function createEditor(slide, label, o = {}) {
 	var lang = o.lang || label;
@@ -18,75 +15,33 @@ function createEditor(slide, label, o = {}) {
 	});
 }
 
-$$(".demo.slide").forEach(slide => slide.classList.add("dont-resize"));
+function scopeRule(rule, slide, scope) {
+	let selector = rule.selectorText;
 
-$$('[data-edit]').forEach(element => {
-	var edit = element.getAttribute("data-edit").split(/\s+/);
-	var slide = element.closest(".slide");
-	var editors = element._.data.editors = {};
-
-	edit.forEach(code => {
-		if (code == "html" || code == "contents") {
-			if (code == "html") {
-				element.removeAttribute("data-edit");
-
-				element = $.create("div", {
-					around: element,
-					"data-edit": edit.join(" ").replace("html", "contents")
-				});
-			}
-
-			editors.contents = createEditor(slide, "contents", {
-				lang: "html",
-				fromSource: () => Prism.plugins.NormalizeWhitespace.normalize(element.innerHTML.replace(/=""(?=\s|>)/g, "")),
-				toSource: function() {
-					element.innerHTML = this.value
-				}
-			});
+	if (rule.cssRules) {
+		// If this rule contains rules, scope those too
+		// Mainly useful for @supports and @media
+		for (let innerRule of rule.cssRules) {
+			scopeRule(innerRule, slide, scope);
 		}
-
-		if (code == "css") {
-			var style = $("style[data-slide]", slide) || $.create("style", {
-				"data-slide": "",
-				inside: slide
-			});
-
-			editors.css = createEditor(slide, "css", {
-				fromSource: () => Prism.plugins.NormalizeWhitespace.normalize(style.textContent),
-				toSource: function() {
-					style.textContent = this.value
-				}
-			});
-		}
-	});
-
-
-
-	if (edit.length > 1) {
-		// More than 1 editors, need the ability to toggle
-		var editorKeys = Object.keys(editors);
-
-		requestAnimationFrame(() => {
-			editorKeys.forEach((label, i) => {
-				var editor = editors[label];
-
-				editor.parentNode.hidden = i > 0;
-
-				$.create("label", {
-					htmlFor: editor.id,
-					after: editor,
-					textContent: editor.dataset.lang,
-					tabIndex: "0",
-					onclick: function() {
-						editor.parentNode.setAttribute("hidden", "");
-						(editors[editorKeys[i+1]] || editors[editorKeys[0]]).parentNode.removeAttribute("hidden");
-					}
-				});
-			});
-		});
-
 	}
-});
+
+	if (selector && rule instanceof CSSStyleRule) {
+		let shouldScope = !(
+			selector.includes("#")  // don't do anything if the selector already contains an id
+			|| selector == ":root"
+		);
+
+		if (selector == "article") {
+			rule.selectorText = `#${slide.id}`;
+		}
+		else if (shouldScope && selector.indexOf(scope) !== 0) {
+			rule.selectorText = scope + " " + selector;
+		}
+	}
+}
+
+$$(".demo.slide").forEach(slide => slide.classList.add("dont-resize"));
 
 (function() {
 	document.addEventListener("keydown", evt => {
@@ -107,83 +62,148 @@ $$('[data-edit]').forEach(element => {
 				}
 
 				editor.style.setProperty("--font-size-multiplier", multiplier);
+
+				resizeTextarea(editor);
 			}
 		}
 	});
 
-	var resize = evt => {
-		var textarea = evt.type == "input"? evt.target : $("textarea.editor", evt.target);
-
-		if (textarea && textarea.matches(".demo.slide.horizontal div.editor > textarea.editor")) {
-			textarea.style.height = "0";
-			textarea.parentNode.style.fontSize = "";
-
-			textarea.style.height = textarea.scrollHeight + "px";
-
-			var cs = getComputedStyle(textarea);
-
-			// if (cs.height == cs.maxHeight) {
-			// 	var ratio = Math.min(2, textarea.scrollHeight/textarea.offsetHeight) - 1;
-			// 	textarea.parentNode.style.fontSize = 100 - Math.round(50 * ratio) + "%";
-			// }
-		}
-	};
-
-	$.events(document.documentElement, "slidechange input", resize);
+	$.events(document.documentElement, "slidechange input", resizeTextarea);
 	window.addEventListener("load", evt => {
-		requestAnimationFrame(() => resize({target: slideshow.currentSlide}));
+		requestAnimationFrame(() => resizeTextarea(slideshow.currentSlide));
 	})
 
 })();
 
-// Display certain keys pressed
-/*
-var keys = {
-	"Tab": 9,
-	"Enter": 13,
-	"Esc": 27,
-	"DownArrow": 40
+function resizeTextarea(textarea) {
+	textarea = textarea.target || textarea;
+
+	if (textarea.nodeName != "TEXTAREA") {
+		textarea = $("textarea.editor", textarea);
+	}
+
+	if (textarea && textarea.matches(".demo.slide.horizontal div.editor > textarea.editor")) {
+		textarea.style.height = "0";
+		textarea.parentNode.style.fontSize = "";
+
+		textarea.style.height = textarea.scrollHeight + "px";
+
+		var cs = getComputedStyle(textarea);
+
+		// if (cs.height == cs.maxHeight) {
+		// 	var ratio = Math.min(2, textarea.scrollHeight/textarea.offsetHeight) - 1;
+		// 	textarea.parentNode.style.fontSize = 100 - Math.round(50 * ratio) + "%";
+		// }
+	}
+};
+
+document.addEventListener("slideshowinit", evt => {
+
+});
+
+function fixHTML(html) {
+	return html.replace(/=""(?=\s|>)/g, "");
 }
 
-var keyCodes = Object.keys(keys).map(key => keys[key]);
+$$('[data-edit]').forEach(element => {
+	var edit = element.getAttribute("data-edit").split(/\s+/);
+	var slide = element.closest(".slide");
+	var editors = element._.data.editors = {};
 
-document.addEventListener("keyup", function(evt) {
-	var code = evt.keyCode;
-	var i = keyCodes.indexOf(code);
+	edit.forEach(code => {
+		if (code == "html" || code == "contents") {
+			if (code == "html") {
+				element.removeAttribute("data-edit");
 
-	if (i > -1 && evt.target.nodeName != "TEXTAREA") {
-		var element = slideshow.currentSlide;
-		var label = Object.keys(keys)[i];
+				element = $.create("div", {
+					around: element,
+					"data-edit": edit.join(" ").replace("html", "contents")
+				});
+			}
 
-		if (element && element.matches(`[data-keys~=${label}]`)) {
-			label = label.replace("Tab", "⇥")
-			             .replace("Enter", "⏎")
-						 .replace("DownArrow", "↓");
-			label = (evt.ctrlKey? "⌃" : "") + (evt.shiftKey? "⇧" : "") + (evt.metaKey? "⌘" : "") + (evt.altKey? "⌥" : "") + label;
+			editors.contents = createEditor(slide, "contents", {
+				lang: "html",
+				fromSource: () => Prism.plugins.NormalizeWhitespace.normalize(fixHTML(element.innerHTML)),
+				toSource: function() {
+					element.innerHTML = this.value
+				}
+			});
+		}
 
-			var key = $.create("kbd", {
-				textContent: label,
-				inside: element
+		if (code == "css") {
+			var style = $("style[data-slide]", slide) || $.create("style", {
+				"data-slide": "",
+				inside: slide
 			});
 
-			setTimeout(() => {
-				$.transition(key, {opacity: 0}).then($.remove);
-			}, 600);
+			editors.css = createEditor(slide, "css", {
+				fromSource: () => Prism.plugins.NormalizeWhitespace.normalize(style.textContent),
+				toSource: function() {
+					style.textContent = this.value;
+
+					if (!style.sheet) {
+						// Stupid Chrome bug
+						style.textContent = style.textContent + "/**/";
+					}
+
+					if (style.sheet) {
+						let scope = style.getAttribute("data-scope") || `#${slide.id}`;
+
+						for (let rule of style.sheet.cssRules) {
+							scopeRule(rule, slide, scope);
+						}
+					}
+					else console.log("FAIL on", slide.id, style.outerHTML, style.media);
+				}
+			});
+
+			slide.addEventListener("slidechange", evt => {
+				editors.css.dispatchEvent(new InputEvent("input"));
+			});
 		}
+	});
+
+	if (edit.length > 1) {
+		// More than 1 editors, need the ability to toggle
+		var editorKeys = Object.keys(editors);
+
+		requestAnimationFrame(() => {
+			editorKeys.forEach((label, i) => {
+				var editor = editors[label];
+
+				editor.parentNode.hidden = i > 0;
+
+				$.create("label", {
+					htmlFor: editor.id,
+					after: editor,
+					textContent: editor.dataset.lang,
+					tabIndex: "0",
+					onclick: function() {
+						editor.parentNode.setAttribute("hidden", "");
+						var newEditor = (editors[editorKeys[i+1]] || editors[editorKeys[0]]);
+						newEditor.parentNode.removeAttribute("hidden");
+
+						resizeTextarea($("textarea", newEditor.parentNode));
+					}
+				});
+			});
+		});
+
 	}
 });
-*/
 
 Prism.languages.insertBefore("css", "property", {
 	"variable": /\-\-(\b|\B)[\w-]+(?=\s*[:,)]|\s*$)/i
 });
 
+// Specific slide demos
+
 document.addEventListener("slidechange", evt => {
 	var slideId = evt.target.id;
 
 	var mousemove = evt => {
-		document.documentElement.style.setProperty("--mouse-x", evt.clientX + "px");
-		document.documentElement.style.setProperty("--mouse-y", evt.clientY + "px");
+		document.documentElement.style.setProperty("--mouse-x", evt.clientX / innerWidth);
+		document.documentElement.style.setProperty("--mouse-y", evt.clientY / innerHeight);
 	}
 
 	if (slideId == "mouse") {
@@ -205,7 +225,6 @@ document.addEventListener("input", evt => {
 });
 
 var setLength = element => {
-	console.log("yolo", element);
 	element.style.setProperty("--length", element.textContent.length)
 };
 
@@ -220,4 +239,18 @@ for (let element of document.querySelectorAll("p.typing")) {
 	});
 }
 
-})(Bliss, Bliss.$);
+$$(".takeaway.slide").forEach((slide, i) => {
+	$.create("span", {
+		className: "label",
+		innerHTML: `Takeaway <span>#</span>${i + 1}`,
+		start: $("h1", slide)
+	});
+});
+
+for (let el of document.querySelectorAll(".scrolling")) {
+	el.addEventListener("scroll", evt => {
+		let maxScroll = el.scrollHeight - el.offsetHeight;
+		let scroll = el.scrollTop / maxScroll;
+		el.style.setProperty("--scroll", scroll);
+	});
+}
