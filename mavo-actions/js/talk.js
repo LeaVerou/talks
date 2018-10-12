aad13dcec02a5f8afb49869eb8ab44982d4ea0f7
Lea Verou <lea@verou.me>
2018-10-12 15:33:15 +0100

    Add click to ... slide

--- mavo-actions/js/talk.js
+++ mavo-actions/js/talk.js
@@ -36,11 +36,11 @@ $("#correctness button").onclick = evt => {
 	var barChart = $(".vertical-bar-chart", slide);
 	var percentSorted = barChart.classList.contains("percent-sort");
 	barChart.classList.toggle("percent-sort", !percentSorted);
-	evt.target.textContent = "Sorted by " + (percentSorted? "order" : "correctness")
+	evt.target.textContent = "Sorted by " + (percentSorted? "order" : "correctness");
 };
 
 $$("#correctness .vertical-bar-chart > div").forEach((div, i) => {
-	var tr = $(`#questions table`).rows[i + 1];
+	var tr = $("#questions table").rows[i + 1];
 
 	$.create({
 		className: "info",
@@ -54,4 +54,46 @@ $$("#correctness .vertical-bar-chart > div").forEach((div, i) => {
 
 $$(".vertical-bar-chart > div").forEach(div => {
 	div.style.setProperty("--order", -1 * Math.round(div.style.getPropertyValue("--p") * 10000));
-})
+});
+
+var rowIncrement;
+document.addEventListener("slidechange", evt => {
+	var slide = evt.target;
+	if (slide.id === "click-to") {
+		var ul = $("ul", slide);
+		var i, lis = $$("li", ul);
+		var rows = lis.length;
+
+		ul.style.setProperty("--rows", rows);
+
+		lis.forEach(li => li.classList.remove("active"));
+
+		var reset = () => {
+			i = 0;
+			ul.style.transition = "0s";
+			setTimeout(() => {
+				ul.style.setProperty("--n", 0);
+				ul.style.transition = "";
+			}, 50);
+		};
+
+		reset();
+
+		(function callee () {
+			if (i > rows) {
+				var last = true;
+				reset();
+			}
+
+			lis[i - 1] && lis[i - 1].classList.remove("active");
+			lis[rows - 1] && lis[rows - 1].classList.remove("active");
+			(lis[i] || lis[0]).classList.add("active");
+			ul.style.setProperty("--n", i++);
+
+			rowIncrement = setTimeout(callee, last? 100 : 800);
+		})();
+	}
+	else if (evt.prevSlide.id === "click-to") {
+		clearInterval(rowIncrement);
+	}
+});
