51f260794ee6677a0ddf405051207ac1788df3e7
Lea Verou <lea@verou.me>
2019-05-15 22:53:06 +0200

    Workshop

--- workshop/talk.js
+++ workshop/talk.js
@@ -3,7 +3,7 @@
 await Inspire.importsLoaded;
 
 if (Inspire.profile !== "speaker") {
-	var allowed = ["header", ".demo", ".takeaway", ".browser-support", "#registerProperty", ".language-javascript", "[mv-app]", ".interactive"];
+	var allowed = ["header", "footer", ".demo", ".takeaway", ".browser-support", "#registerProperty", ".language-javascript", "[mv-app]", ".interactive"];
 
 	$$(".slide" + allowed.map(s => `:not(${s})`).join("") + ", .speaker-only").forEach(slide => slide.remove());
 }
