b9fae6366056e8d9bddcd42c9758244f42c98959
Lea Verou <lea@verou.me>
2026-09-04 23:48:41 +0300

    WIP

--- no-build/talk.js
+++ no-build/talk.js
@@ -2,10 +2,30 @@ import "./prism.js";
 import Inspire from "inspirejs.org";
 
 /* Load code samples from external files: <pre data-src="foo.css"> */
-for (let pre of document.querySelectorAll("pre[data-src]")) {
-	let src = pre.dataset.src;
-	fetch(src).then(r => r.text()).then(text => {
-		pre.innerHTML = `<code>${text}</code>`;
-		Prism.highlightElement(pre);
-	});
+// for (let pre of document.querySelectorAll("pre[data-src]")) {
+// 	let src = pre.dataset.src;
+// 	fetch(src).then(r => r.text()).then(text => {
+// 		pre.innerHTML = `<code>${text}</code>`;
+// 		Prism.highlightElement(pre);
+// 	});
+// }
+await Inspire.importsLoaded;
+await Inspire.plugins.loaded.markdown.loaded;
+console.log(document.querySelectorAll("ul.file-tree li"));
+for (let li of document.querySelectorAll("ul.file-tree li")) {
+
+	let content = li.firstChild.textContent.trim();
+	console.log(content);
+	if (content.endsWith("/")) {
+		li.classList.add("folder");
+	}
+	else if (content.startsWith(".")) {
+		li.classList.add("hidden");
+	}
+	else {
+		let ext = content.match(/\.(\w+)$/);
+		if (ext) {
+			li.classList.add(ext[1]);
+		}
+	}
 }
